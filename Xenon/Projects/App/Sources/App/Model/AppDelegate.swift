b4e874c79d5f811b31b03b1e42fad19769bcba9c
tkgka <ksh17171@gmail.com>
2025-03-03 22:56:36 +0900

    [] fix loading effect

--- Xenon/Projects/App/Sources/App/Model/AppDelegate.swift
+++ Xenon/Projects/App/Sources/App/Model/AppDelegate.swift
@@ -28,14 +28,16 @@ class AppDelegate: NSObject, UIApplicationDelegate {
             loadingViewController = UIViewController()
             guard let loadingViewController else { return }
             loadingViewController.view.backgroundColor = UIColor.black.withAlphaComponent(0.2)
-            
-            let rectangle = UIView()
-            rectangle.backgroundColor = UIColor.black.withAlphaComponent(0.5)
-            rectangle.layer.cornerRadius = 16
-            rectangle.frame.size = .init(width: 120, height: 120)
-            rectangle.center = loadingViewController.view.center
 
-            loadingViewController.view.addSubview(rectangle)
+            
+            let blurEffect = UIBlurEffect(style: .systemThinMaterial)
+            let visualEffectView = UIVisualEffectView(effect: blurEffect)
+            visualEffectView.frame.size = .init(width: 120, height: 120)
+            visualEffectView.center = loadingViewController.view.center
+            visualEffectView.layer.cornerRadius = 24
+            visualEffectView.clipsToBounds = true
+            loadingViewController.view.addSubview(visualEffectView)
+            
             let spinner = UIActivityIndicatorView(style: .large)
             spinner.center = loadingViewController.view.center
             spinner.startAnimating()
