159feee77a54006011758dac8e00ec1069784ec0
tkgka <ksh17171@gmail.com>
2025-03-09 17:56:17 +0900

    [] edit project settings

--- Xenon/Projects/App/Project.swift
+++ Xenon/Projects/App/Project.swift
@@ -5,12 +5,8 @@ import EnvPlugin
 
 let targets: [Target] = [
     .makeApp(name: ProjectInfo.name, dependencies: [
-        ModulePaths.feature(.RSSFeature).dependency,
         ModulePaths.feature(.FediverseFeature).dependency,
         ModulePaths.feature(.UIComponent).dependency,
-        ModulePaths.feature(.NetworkingFeature).dependency,
-        .external(name: "Neumorphic"),
-        .external(name: "Kingfisher"),
     ])
 ].addTest()
 
