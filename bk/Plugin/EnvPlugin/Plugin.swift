f4369b4a22a2e1f3d57917ab006e88cd6df148f5
tkgka <ksh17171@gmail.com>
2026-01-21 21:29:00 +0900

    [] remove bk files

--- bk/Plugin/EnvPlugin/Plugin.swift
+++
@@ -1,3 +0,0 @@
-import ProjectDescription
-
-let envPlugin = Plugin(name:"EnvPlugin")
--- bk/Tuist.swift
+++
@@ -1,8 +0,0 @@
-import ProjectDescription
-
-let tuist = Tuist(
-    plugins: [
-        .local(path: .relativeToRoot("Plugin/EnvPlugin")),
-        .local(path: .relativeToRoot("Plugin/UtilityPlugin"))
-    ]
-)
